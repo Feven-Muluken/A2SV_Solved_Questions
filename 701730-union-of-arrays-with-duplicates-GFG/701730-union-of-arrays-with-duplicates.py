c3f4eeb3ddce8311952b828a01a40dc91865a3fd
Feven Muluken <167541084+Feven-Muluken@users.noreply.github.com>
2026-02-04 20:37:55 +0300

    Added solution - LeetHub

--- 701730-union-of-arrays-with-duplicates-GFG/701730-union-of-arrays-with-duplicates.py
+++ 701730-union-of-arrays-with-duplicates-GFG/701730-union-of-arrays-with-duplicates.py
@@ -2,14 +2,3 @@ class Solution:
     def findUnion(self, a, b):
         # code here
         return set(a + b)
-        # new_array = []
-        # for i in a:
-        #     if i in new_array:
-        #         continue
-        #     new_array.append(i)
-        # for j in b:
-        #     if j in new_array:
-        #         continue
-        #     new_array.append(j)
-            
-        # return sorted(new_array)
