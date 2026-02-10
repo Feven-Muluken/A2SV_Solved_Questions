84d8d82b6366f74e4808f034a93ca7e1338cfa22
Feven Muluken <167541084+Feven-Muluken@users.noreply.github.com>
2026-02-10 11:37:26 +0300

    Time: 8 ms (13.06%), Space: 19.3 MB (52.65%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        ans = []
+        for i in cpdomains:
+            a = i.split(' ')
+            b = a[1].split('.')
+            l = 0
+            while l < len(b):
+                all_doms = a[0] + ' ' + ".".join(b[l:]) 
+                ans.append(all_doms)
+                l += 1
+
+        counts = {}
+        for web in ans:
+            count, domain = web.split()
+            count = int(count)
+            counts[domain] = counts.get(domain, 0) + count
+        answer = [f'{total} {dom}' for dom, total in counts.items()]
+        return answer
