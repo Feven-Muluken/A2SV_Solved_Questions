812d3024d6d944922616c6e58aab6d92bcd33acd
Feven Muluken <167541084+Feven-Muluken@users.noreply.github.com>
2026-04-24 16:39:28 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        count_5, count_10 = 0, 0
+        for i in bills:
+            if i == 5:
+                count_5 += 1
+            elif i == 10:
+                if count_5 == 0:
+                    return False
+                count_5 -= 1
+                count_10 += 1
+            else:
+                if count_10 > 0 and count_5 > 0:
+                    count_10 -= 1
+                    count_5 -= 1
+                elif count_5 >= 3:
+                    count_5 -+ 3
+                else:
+                    return False
+        return True
